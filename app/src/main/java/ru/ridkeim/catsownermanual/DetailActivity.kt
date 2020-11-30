6d32dc519d0a1f9318d17ff85e1453b0c0f381f8
ridkeim <ridkeim@gmail.com>
2020-11-30 11:58:57 +0300

    Добавлены файлы глав и их обработка

--- app/src/main/java/ru/ridkeim/catsownermanual/DetailActivity.kt
+++ app/src/main/java/ru/ridkeim/catsownermanual/DetailActivity.kt
@@ -1,8 +1,12 @@
 package ru.ridkeim.catsownermanual
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
 
 class DetailActivity : AppCompatActivity() {
 
@@ -17,7 +21,34 @@ class DetailActivity : AppCompatActivity() {
         val chapterTitle = intent.getStringExtra(KEY_CHAPTER_TITLE) ?: ""
         val chapterNumber = intent.getIntExtra(KEY_CHAPTER_NUMBER,0)+1
         val webView = findViewById<WebView>(R.id.webView)
-        val text = "$chapterNumber $chapterTitle"
+        val text = readChapterFile(this,chapterNumber,chapterTitle)
         webView.loadDataWithBaseURL(null, text, "text/html", "en_US", null)
+    }
+
+    private fun readChapterFile(context : Context, chapterNumber : Int, chapterTitle : String) : String{
+        val chapterFileName = "chapter$chapterNumber"
+        val chapterFileId =
+            context.resources.getIdentifier(
+                chapterFileName,
+                "raw",
+                "ru.ridkeim.catsownermanual"
+            )
+        val stringBuilder = StringBuilder()
+        stringBuilder.append("<h1>Глава $chapterNumber. $chapterTitle</h1>")
+        if(chapterFileId!=0){
+            BufferedReader(
+                InputStreamReader(
+                    context.resources.openRawResource(chapterFileId)
+                )
+            ).useLines{
+                it.forEach {line ->
+                    stringBuilder.append(line)
+                    stringBuilder.append("<br>")
+                }
+            }
+        } else{
+            stringBuilder.append("Отсутствует файл $chapterFileName")
+        }
+        return stringBuilder.toString()
     }
 }
